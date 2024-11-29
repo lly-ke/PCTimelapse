57a538aea4eb3b700bd0202be7605b641d852c67
今天你写bug了吗 <2720851545@qq.com>
2024-11-29 14:49:09 +0800

    Initial Commit

---
+++ GazeTracker/GazeTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GazeTrackerApp.swift
+//  GazeTracker
+//
+//  Created by llyke on 2024/11/29.
+//
+
+import SwiftUI
+
+@main
+struct GazeTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GazeTrackerTests/GazeTrackerTests.swift
@@ -0,0 +1,16 @@
+//
+//  GazeTrackerTests.swift
+//  GazeTrackerTests
+//
+//  Created by llyke on 2024/11/29.
+//
+
+import Testing
+
+struct GazeTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
